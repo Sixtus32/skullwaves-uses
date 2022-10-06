6447b6ae11d5af21d38e977e9ab77d766605aec2
Sixtus Nosike <sixtus@LAPTOP-81T2M9VJ.localdomain>
2022-10-06 16:23:38 +0200

    toggling theme

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,8 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'skullwaves - blog';
+export const SITE_TITLE = 'skullwaves';
 export const SITE_DESCRIPTION = 'A daily blog about tech and what I am learning.';
 export const HOMEPAGE_URL = 'https://skullwaves.io';
+export const GITHUB_URL = 'https://github.com/Sixtus32';
 
