2dd7695a823c88c77de7d737e33de28eec04016a
Sixtus Nosike <sixtus@LAPTOP-81T2M9VJ.localdomain>
2022-10-06 20:31:09 +0200

    adding introduccion content

--- src/config.ts
+++ src/config.ts
@@ -5,4 +5,4 @@ export const SITE_TITLE = 'skullwaves';
 export const SITE_DESCRIPTION = 'A daily blog about tech and what I am learning.';
 export const HOMEPAGE_URL = 'https://skullwaves.io';
 export const GITHUB_URL = 'https://github.com/Sixtus32';
-
+export const NAME = 'skullwaves';
